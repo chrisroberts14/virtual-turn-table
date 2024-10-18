6b0bd07af91e9e65c349f593a1549b62820f3a04
chris <chrisroberts1080@gmail.com>
2024-10-18 11:50:39 +0100

    feat: camera scaling with resolution

--- turn_table_ui/src/components/ScanPage.tsx
+++ turn_table_ui/src/components/ScanPage.tsx
@@ -1,16 +1,35 @@
 import { Button } from "@nextui-org/button";
 import { Resizable } from "re-resizable";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Webcam from "react-webcam";
+
+type MediaDeviceInfo = {
+	kind: string;
+	label: string;
+	deviceId: string;
+	groupId: string;
+};
 
 const ScanPage = () => {
 	const [contentHeight, setContentHeight] = useState(window.innerHeight - 240);
+	const [cameras, setCameras] = useState<MediaDeviceInfo[]>([]);
 
 	const onResize = () => {
 		setContentHeight(window.innerHeight - 240);
 	};
 
 	window.addEventListener("resize", onResize);
+
+	// Use callback to cache the function between refreshes
+	const getCameras = useCallback((mediaDevices: MediaDeviceInfo[]) => {
+		setCameras(mediaDevices.filter(({ kind }) => kind === "videoinput"));
+	}, []);
+
+	useEffect(() => {
+		navigator.mediaDevices.enumerateDevices().then((devices) => {
+			getCameras(devices as MediaDeviceInfo[]);
+		});
+	}, [getCameras]);
 
 	return (
 		<div className="flex flex-col h-full">
@@ -42,24 +61,36 @@ const ScanPage = () => {
 							topLeft: false,
 						}}
 						minWidth={"20%"}
+						maxWidth={"45%"}
 					>
 						<div className="overflow-y-auto h-full">Song Confirm</div>
 					</Resizable>
 					<div className="flex-grow p-3 max-w-[65%]">
-						<Webcam
-							audio={false}
-							height={720}
-							screenshotFormat="image/jpeg"
-							width={1920}
-						/>
-					</div>
-					<div className="p-4">
-						<Button>Upload picture</Button>
+						{cameras.length === 0 ? (
+							<div>No cameras found</div>
+						) : (
+							<div className="flex justify-center max-h-full p-32 flex-col">
+								<Webcam
+									audio={false}
+									height={1080}
+									screenshotFormat="image/jpeg"
+									width={1920}
+									className="rounded-lg"
+								/>
+								<div className="p-4 text-center min-w-[20%] justify-center">
+									<Button>Upload picture</Button>
+								</div>
+							</div>
+						)}
 					</div>
 				</div>
 			</Resizable>
 			<div className="flex-grow bg-primary-100 flex justify-center pt-2 w-screen">
-				Footer
+				<div className="flex flex-col min-w-[30%]">
+					<div>Previously scanned albums:</div>
+					<div>Selected album</div>
+				</div>
+				<div className="flex-grow">Album selection</div>
 			</div>
 		</div>
 	);
