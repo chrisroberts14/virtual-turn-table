0db10fecd12c14abf8ab77ec9fd735d9886384af
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-10-29 12:22:02 +0000

    test: add app tests

---
+++ turn_table_ui/__tests__/hooks/UseApp.test.tsx
@@ -0,0 +1,76 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { act } from "react";
+import { vi } from "vitest";
+import useApp from "../../src/hooks/UseApp";
+import type Album from "../../src/interfaces/Album";
+import { getStateData } from "../../src/interfaces/StateData";
+
+vi.mock("../../src/interfaces/StateData");
+
+describe("UseApp", () => {
+	it("should initialise properly when not logged in", () => {
+		// @ts-ignore
+		(getStateData as vi.mock).mockReturnValue({ currentPage: "play" });
+		const { result } = renderHook(() => useApp());
+		expect(result.current.isSignedIn).toBe(false);
+		expect(result.current.nextPage).toBe("play");
+		expect(result.current.token).toBe(null);
+	});
+
+	it("should initialise properly when logged in with valid token", () => {
+		// @ts-ignore
+		(getStateData as vi.mock).mockReturnValue({
+			currentPage: "play",
+			spotify_access_token: "test_token",
+			spotify_session_length: "3600",
+			spotify_login_time: new Date().toISOString(),
+		});
+		const { result } = renderHook(() => useApp());
+		expect(result.current.isSignedIn).toBe(true);
+		expect(result.current.nextPage).toBe("play");
+		expect(result.current.token).toBe("test_token");
+	});
+
+	it("should initialise properly when params are supplied in the URL", () => {
+		const originalLocation = window.location;
+		Object.defineProperty(window, "location", {
+			value: {
+				hash: "?access_token=test_token&expires_in=3600",
+			},
+			writable: true,
+		});
+		const { result } = renderHook(() => useApp());
+		expect(result.current.isSignedIn).toBe(true);
+		expect(result.current.token).toBe("test_token");
+		Object.defineProperty(window, "location", {
+			value: originalLocation,
+			writable: true,
+		});
+	});
+
+	it("should initialise properly when the session has expired", () => {
+		const pastDate: Date = new Date(99);
+		// @ts-ignore
+		(getStateData as vi.mock).mockReturnValue({
+			currentPage: "play",
+			spotify_access_token: "test_token",
+			spotify_session_length: "3600",
+			spotify_login_time: pastDate.toISOString(),
+		});
+		const { result } = renderHook(() => useApp());
+		waitFor(() => {
+			expect(result.current.isSignedIn).toBe(false);
+			expect(result.current.token).toBe(null);
+		});
+	});
+
+	it("should move to play page when currentAlbum is changed", () => {
+		const { result } = renderHook(() => useApp());
+		act(() => {
+			result.current.setCurrentAlbum({} as Album);
+		});
+		waitFor(() => {
+			expect(result.current.nextPage).toBe("play");
+		});
+	});
+});
---
+++ turn_table_ui/src/hooks/UseApp.tsx
@@ -0,0 +1,107 @@
+import type Album from "@/interfaces/Album.tsx";
+import {
+	clearStateData,
+	getStateData,
+	storeStateData,
+} from "@/interfaces/StateData.tsx";
+import { useEffect, useState } from "react";
+
+const useApp = () => {
+	const [isSignedIn, setIsSignedIn] = useState(false);
+	const [currentAlbum, setCurrentAlbum] = useState<Album | null>(null);
+	const [token, setToken] = useState<string | null>(null);
+	// Two pages are defined in the state: "play" and "scan"
+	const [nextPage, setNextPage] = useState("");
+	const [fadeScan, setFadeScan] = useState(false);
+	const [fadePlayer, setFadePlayer] = useState(false);
+	const [disableTabChange, setDisableTabChange] = useState(false);
+
+	useEffect(() => {
+		// Set to the correct page
+		const currentState = getStateData();
+		if (currentState?.currentPage) {
+			setNextPage(currentState.currentPage);
+		}
+
+		const hash = window.location.hash.substring(1);
+		const params = new URLSearchParams(hash);
+		const token = params.get("access_token");
+
+		if (token) {
+			storeStateData({
+				spotify_access_token: token,
+				spotify_login_time: String(new Date()),
+				spotify_session_length: params.get("expires_in") as string,
+			});
+			setIsSignedIn(true);
+			setToken(token);
+			window.location.href = "/";
+		} else {
+			if (currentState?.spotify_access_token) {
+				if (
+					currentState.spotify_session_length &&
+					currentState.spotify_login_time
+				) {
+					const currentSessionLength =
+						(new Date().getTime() -
+							new Date(currentState.spotify_login_time).getTime()) /
+						1000;
+
+					if (
+						currentSessionLength > Number(currentState.spotify_session_length)
+					) {
+						clearStateData();
+						setIsSignedIn(false);
+						setToken(null);
+					} else {
+						setIsSignedIn(true);
+						setToken(currentState.spotify_access_token);
+					}
+				}
+			} else {
+				setIsSignedIn(false);
+				setToken(null);
+			}
+		}
+	}, []);
+
+	useEffect(() => {
+		triggerScreenChange(nextPage);
+		storeStateData({ currentPage: nextPage });
+	}, [nextPage]);
+
+	useEffect(() => {
+		if (currentAlbum) {
+			setNextPage("play");
+		}
+	}, [currentAlbum]);
+
+	const triggerScreenChange = (page: string) => {
+		setDisableTabChange(true);
+		if (page === "play") {
+			setFadeScan(false);
+			setFadePlayer(true);
+		} else {
+			setFadePlayer(false);
+			setFadeScan(true);
+		}
+		setDisableTabChange(false);
+	};
+
+	return {
+		isSignedIn,
+		setIsSignedIn,
+		currentAlbum,
+		setCurrentAlbum,
+		token,
+		setToken,
+		nextPage,
+		setNextPage,
+		fadeScan,
+		fadePlayer,
+		disableTabChange,
+		setDisableTabChange,
+	};
+};
+
+export default useApp;
